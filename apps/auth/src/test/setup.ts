1f5b4a347a073cccfa0e3689eaf81a09f93393a6
Nikolay Petrov <nikolay.petrov@abv.bg>
2024-02-21 16:44:45 +0200

    add initial jest test setup

---
+++ apps/auth/src/test/setup.ts
@@ -0,0 +1,32 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+
+// change if initial mongo download takes longer
+jest.setTimeout(20000);
+
+let mongo: any;
+beforeAll(async () => {
+  process.env.JWT_KEY = 'asdfasdf';
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  // reset all data in db
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
